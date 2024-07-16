9cc808322aec48b791c2cb45bf30e34076cc9199
haddedseif746@gmail.com <haddedseif746@gmail.com>
2024-07-16 02:07:31 +0200

    first commit

---
+++ android/app/src/main/java/com/example/chat_app/MyApplication.java
@@ -0,0 +1,11 @@
+package com.example.chat_app;
+
+import androidx.multidex.MultiDexApplication;
+
+public class MyApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Custom initialization if necessary
+    }
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+// import 'package:udemy_shop_app/logic/bindings/auth_biniding.dart';
+// import 'package:udemy_shop_app/logic/bindings/main_binding.dart';
+// import 'package:udemy_shop_app/logic/bindings/product_binding.dart';
+// import 'package:udemy_shop_app/logic/controllers/product_controller.dart';
+import 'package:chat_app/view/screens/auth/forget_password_screen.dart';
+import 'package:chat_app/view/screens/auth/login_screen.dart';
+import 'package:chat_app/view/screens/auth/signup_screen.dart';
+import 'package:chat_app/view/screens/main_screen.dart';
+
+import 'package:chat_app/view/screens/welcome_screen.dart';
+
+class AppRoutes {
+  //initialRoute
+
+  static const welcome = Routes.welcomeScreen;
+
+  //getPages
+
+  static final routes = [
+    GetPage(
+      name: Routes.welcomeScreen,
+      page: () => const WelcomeScreen(),
+    ),
+  ];
+}
+
+class Routes {
+  static const welcomeScreen = '/welcomeScreen';
+}
